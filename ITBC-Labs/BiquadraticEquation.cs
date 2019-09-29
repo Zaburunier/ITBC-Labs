433d3088a5b0be99c3d884cf8478039ca5d2fb82
lenyazaburunov <lenyazaburunov@bk.ru>
2019-09-29 17:54:40 +0300

    Всё, кроме обработки некорректного ввода коэффицентов

---
+++ ITBC-Labs/BiquadraticEquation.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITBC_Labs
+{
+    class BiquadraticEquation
+    {
+        private int A, B, C;
+        private double[] solution;
+        public BiquadraticEquation(int[] values)
+        {
+            this.A = values[0];
+            this.B = values[1];
+            this.C = values[2];
+        }
+        public void SolveEquation()
+        {
+            if (this.CheckForLackOfSolutions())//Проверка на наличие решений
+            {//Если их нет
+                Console.WriteLine("Данное уравнение не имеет действительных решений.");
+                this.solution = null;
+            } else {//Если они есть
+                if (this.A == 0 || this.B == 0 || this.C == 0) this.IncompleteEquation();
+                //Если хотя бы один из коэффицентов занулён, то решение уравнения сводится к поиску корней в обход дискриминанта
+                else this.CompleteEquation();
+            }
+        }
+
+        private bool CheckForLackOfSolutions()//Возвращает истину, если решений уравнения нет
+        {
+            if (this.A >= 0 && this.B >= 0 && this.C >= 0) return true;
+            if (this.A <= 0 && this.B <= 0 && this.C <= 0) return true;
+            //Если все коэффиценты имеют одинаковый знак (в обоих случаях допускается ноль), то действительных решений у такого биквадратного уравнения не существует из-за чётности степеней аргумента
+            if (this.A == 0 && this.B == 0 && this.C != 0) return true;
+            //1 уравнение, 0 переменных => решений нет
+            return false;
+        }
+
+        private void IncompleteEquation()//Решение неполного уравнения, где хотя бы один коэффицент - ноль
+        {
+            if (this.A == 0 && this.B != 0 && this.C != 0)
+            {//B * x^2 + C = 0
+                double temp = (double)C / -B;//Значение x^2
+                if (temp > 0) {
+                    this.solution = new double[2] { Math.Sqrt(temp), -Math.Sqrt(temp) };
+                } else this.solution = null;
+                return;
+            }
+
+            if (this.C == 0 && (this.A == 0 || this.B == 0))
+            {//B * x^2 = 0 или A * x^4 = 0
+                this.solution = new double[1] { 0 };
+                return;
+            }
+
+            if (this.A != 0 && this.B != 0 && this.C == 0)
+            {//A * x^4 + B * x^2 = 0
+                double temp = (double)B / -A;
+                if (temp > 0)
+                {
+                    this.solution = new double[3] { 0, Math.Sqrt(temp), -Math.Sqrt(temp)};
+                } else
+                {
+                    this.solution = new double[1] { 0 };
+                }
+                return;
+            }
+
+            if (this.A != 0 && this.B == 0 && this.C != 0)
+            {//A * x^4 + C = 0
+                double temp = (double)C / -A;
+                if (temp > 0)
+                {
+                    this.solution = new double[2] { Math.Sqrt(Math.Sqrt(temp)), -Math.Sqrt(Math.Sqrt(temp)) };
+                } else this.solution = null;
+                return;
+            }
+        }
+
+        private void CompleteEquation()//Решение полного уравнения
+        {
+            double dscr = this.Discriminant();
+            double t1 = (-this.B + Math.Sqrt(dscr)) / (2 * this.A),
+                   t2 = (-this.B - Math.Sqrt(dscr)) / (2 * this.A);
+            if (t1 > 0)
+            {
+                if (t2 > 0)
+                {
+                    this.solution = new double[4] { Math.Sqrt(t1), -Math.Sqrt(t1), Math.Sqrt(t2), -Math.Sqrt(t2) };
+                    return;
+                }
+
+                if (t2 == 0)
+                {
+                    this.solution = new double[3] { Math.Sqrt(t1), -Math.Sqrt(t1), 0 };
+                    return;
+                }
+
+                if (t2 < 0)
+                {
+                    this.solution = new double[2] { Math.Sqrt(t1), -Math.Sqrt(t1) };
+                    return;
+                }
+            }
+
+            if (t1 == 0)
+            {
+                if (t2 > 0)
+                {
+                    this.solution = new double[3] { Math.Sqrt(t2), -Math.Sqrt(t2), 0 };
+                    return;
+                }
+
+                if (t2 <= 0)
+                {
+                    this.solution = new double[1] { 0 };
+                    return;
+                }
+            }
+
+            if (t1 < 0)
+            {
+                if (t2 > 0)
+                {
+                    this.solution = new double[2] { Math.Sqrt(t2), -Math.Sqrt(t2) };
+                    return;
+                }
+
+                if (t2 == 0)
+                {
+                    this.solution = new double[1] { 0 };
+                    return;
+                }
+
+                if (t2 < 0)
+                {
+                    this.solution = null;
+                    return;
+                }
+            }
+        }
+
+
+        private float Discriminant()
+        {
+            return this.B * this.B - 4 * this.A * this.C;
+        }
+
+        public void Answer()
+        {
+            if (this.solution == null)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Данное уравнение не имеет действительных решений. ");
+                Console.ResetColor();
+                return;
+            }
+
+            Console.WriteLine("Список решений для уравнения: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            for (int i = 0; i < this.solution.Length; i++)
+            {
+                Console.WriteLine(this.solution[i]);
+            }
+            Console.ResetColor();
+        }
+    }
+}
--- ITBC-Labs/Program.cs
+++ ITBC-Labs/Program.cs
@@ -10,6 +10,35 @@ namespace ITBC_Labs
     {
         static void Main(string[] args)
         {
+            Console.Title = "Лабораторная работа №1. Забурунов Леонид, РТ5-31-2019";
+            Console.WriteLine("Данная программа предназначена для решения уравнения вида A * x^4 + B  * x^2 + C = 0. ");
+            GetNumbers();
+            Console.ReadKey();
+            return;
+        }
+
+        static void GetNumbers()
+        {
+            string answer;
+            do
+            {
+                int[] rates = new int[3];
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine("Введите коэффицент " + Convert.ToChar(65 + i) + ": ");
+                    rates[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                Solutions(rates);
+                Console.WriteLine("Хотите продолжить? (1, если хотите)");
+                answer = Console.ReadLine();
+            } while (answer == "1");
+        }
+
+        static void Solutions(int[] rates)
+        {
+            BiquadraticEquation equation = new BiquadraticEquation(rates);
+            equation.SolveEquation();
+            equation.Answer();
         }
     }
 }
